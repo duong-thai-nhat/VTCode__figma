6991f539cd97e638f53c22f3971a31684610655f
duong-thai-nhat <63115734+duong-thai-nhat@users.noreply.github.com>
2022-05-12 16:42:17 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// var flkty = new Flickity('.carousel');
+
+// var buttonGroup = document.querySelector('.carousel');
+// var buttons = buttonGroup.querySelectorAll('.carousel-cell');
+// buttons = fizzyUIUtils.makeArray( buttons );
+
+// buttonGroup.addEventListener( 'click', function( event ) {
+//   // filter for button clicks
+//   if ( !matchesSelector( event.target, '.carousel-cell' ) ) {
+//     return;
+//   }
+//   var index = buttons.indexOf( event.target );
+//   flkty.select( index );
+// });
